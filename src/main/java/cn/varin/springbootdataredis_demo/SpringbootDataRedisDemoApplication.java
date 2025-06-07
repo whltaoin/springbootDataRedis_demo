36542edcb616fb48b215225e6cd44d433374e2d7
Var_ya <whltaoin@163.com>
2025-06-07 14:27:20 +0800

    project init

---
+++ src/main/java/cn/varin/springbootdataredis_demo/SpringbootDataRedisDemoApplication.java
@@ -0,0 +1,13 @@
+package cn.varin.springbootdataredis_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDataRedisDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDataRedisDemoApplication.class, args);
+	}
+
+}
