fbecddd4ed465890cab853e3ab850b8b28ccf88d
Var_ya <whltaoin@163.com>
2025-06-07 15:22:57 +0800

    String type basic example

--- src/main/java/cn/varin/springbootdataredis_demo/pojo/User.java
+++ src/main/java/cn/varin/springbootdataredis_demo/pojo/User.java
@@ -1,2 +1,43 @@
-package cn.varin.springbootdataredis_demo.pojo;public class User {
+package cn.varin.springbootdataredis_demo.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+public class User {
+     String name;
+     Integer age;
+     public User(){
+
+     }
+     public User(String name,Integer age){
+          this. name = name;
+          this.age = age;
+
+     }
+
+     public void setAge(Integer age) {
+          this.age = age;
+     }
+
+     public void setName(String name) {
+          this.name = name;
+     }
+
+     public Integer getAge() {
+          return age;
+     }
+
+     public String getName() {
+          return name;
+     }
+
+     @Override
+     public String toString() {
+          return "User{" +
+                  "name='" + name + '\'' +
+                  ", age=" + age +
+                  '}';
+     }
 }
--- src/test/java/cn/varin/springbootdataredis_demo/SpringbootDataRedisDemoApplicationTests.java
+++ src/test/java/cn/varin/springbootdataredis_demo/SpringbootDataRedisDemoApplicationTests.java
@@ -1,13 +1,26 @@
 package cn.varin.springbootdataredis_demo;
 
+import cn.varin.springbootdataredis_demo.pojo.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 
 @SpringBootTest
 class SpringbootDataRedisDemoApplicationTests {
 
+	@Autowired
+	RedisTemplate redisTemplate;
 	@Test
-	void contextLoads() {
+	void setTest() {
+
+		ValueOperations valueOperations = redisTemplate.opsForValue();
+		valueOperations.set("user:1",new User("varin",1).toString());
+		Object o = valueOperations.get("user:1");
+		System.out.println(o);
+
+
 	}
 
 }
