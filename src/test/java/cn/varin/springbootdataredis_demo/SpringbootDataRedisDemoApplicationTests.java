29187ddbca39570dc6e2e8134e54872ac8df5cf1
Var_ya <whltaoin@163.com>
2025-06-07 16:23:27 +0800

    StringredisTemplate class test

--- src/test/java/cn/varin/springbootdataredis_demo/SpringbootDataRedisDemoApplicationTests.java
+++ src/test/java/cn/varin/springbootdataredis_demo/SpringbootDataRedisDemoApplicationTests.java
@@ -1,11 +1,16 @@
 package cn.varin.springbootdataredis_demo;
 
 import cn.varin.springbootdataredis_demo.pojo.User;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
+
+import java.util.Stack;
 
 @SpringBootTest
 class SpringbootDataRedisDemoApplicationTests {
@@ -29,6 +34,27 @@ class SpringbootDataRedisDemoApplicationTests {
 		valueOperations.set("user:2",new User("varin",1));
 		Object o = valueOperations.get("user:1");
 		System.out.println(o);
+
+
+	}
+
+	@Autowired
+	StringRedisTemplate stringRedisTemplate;
+	// 用于转Json格式
+	private static  final ObjectMapper mapper = new ObjectMapper();
+	@Test
+	void StringRedisTamplateTest() throws JsonProcessingException {
+		User user = new User("varya",1);
+		// 转JSON格式
+		String s = mapper.writeValueAsString(user);
+		// 写入数据
+		stringRedisTemplate.opsForValue().set("user:3",s);
+
+		// 读取数据
+		String s1 = stringRedisTemplate.opsForValue().get("user:3");
+		// 反序列化
+		User user1 = mapper.readValue(s1, User.class);
+		System.out.println(user1);
 
 
 	}
